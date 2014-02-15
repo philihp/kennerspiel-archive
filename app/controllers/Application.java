ade4730ea77abeb8c85d527cfa36e6a8c6fa5e87
Philihp Busby <philihp@gmail.com>
2014-02-15 03:23:20 -0500

    added in user logins. basically whatever the docs said to do

--- app/controllers/Application.java
+++ app/controllers/Application.java
@@ -10,8 +10,9 @@ import views.html.*;
 
 public class Application extends Controller {
 
+	@Security.Authenticated(Secured.class)
 	public static Result index() {
-		return ok(index.render());
+		return ok(index.render(User.find.byId(request().username())));
 	}
 
 	public static Result login() {
