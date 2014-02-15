63f8b5cfa88679077520d690efc03703d787bf6b
Philihp Busby <philihp@gmail.com>
2014-02-15 17:52:35 -0500

    started work on creating instance

--- app/controllers/Application.java
+++ app/controllers/Application.java
@@ -1,10 +1,13 @@
 package controllers;
 
+import java.util.Date;
+
 import models.*;
 import game.Board;
 import play.*;
 import play.mvc.*;
 import play.data.*;
+import play.data.validation.Constraints.Required;
 
 import views.html.*;
 
@@ -33,27 +36,48 @@ public class Application extends Controller {
 	public static Result register() {
 		return ok(register.render(Form.form(Register.class)));
 	}
-	
+
 	public static Result registerSubmit() {
-		Form<Register> registerForm = Form.form(Register.class).bindFromRequest();
-		if(registerForm.hasErrors()) {
+		Form<Register> registerForm = Form.form(Register.class)
+				.bindFromRequest();
+		if (registerForm.hasErrors()) {
 			return badRequest(register.render(registerForm));
-		}
-		else {
-			new User(registerForm.get().email,
-					registerForm.get().name,
-					registerForm.get().password
-					).save();
+		} else {
+			new User(registerForm.get().email, registerForm.get().name,
+					registerForm.get().password).save();
 			session().clear();
 			session("email", registerForm.get().email);
 			return redirect(routes.Application.index());
 		}
 	}
 
+	@Security.Authenticated(Secured.class)
 	public static Result logout() {
 		session().clear();
 		flash("success", "You've been logged out");
 		return redirect(routes.Application.login());
+	}
+
+	@Security.Authenticated(Secured.class)
+	public static Result createInstance() {
+		System.out.println(User.find.byId(request().username()).name);
+		return ok(createInstance.render(Form.form(CreateInstance.class), User.find.byId(request().username())));
+	}
+
+	@Security.Authenticated(Secured.class)
+	public static Result createInstanceSubmit() {
+		Form<CreateInstance> createInstanceForm = Form.form(CreateInstance.class).bindFromRequest();
+		if(createInstanceForm.hasErrors()) {
+			return badRequest(createInstance.render(createInstanceForm, User.find.byId(request().username())));
+		}
+		else {
+			Instance instance = new Instance();
+			instance.seed = (new Date()).getTime();
+			instance.name = createInstanceForm.get().name;
+			instance.game = createInstanceForm.get().game;
+			instance.save();
+			return redirect(routes.Application.index());
+		}
 	}
 
 	public static class Login {
@@ -80,5 +104,14 @@ public class Application extends Controller {
 			}
 			return null;
 		}
+	}
+	
+	public static class CreateInstance {
+		
+		@Required
+		public String name;
+		
+		@Required
+		public String game;
 	}
 }
