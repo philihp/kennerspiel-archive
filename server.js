f5dd1b928ad70d253f540928349ee356769ce7b1
Philihp Busby <philihp@gmail.com>
2015-09-15 01:50:54 -0700

    Use the right port

--- server.js
+++ server.js
@@ -1,12 +1,13 @@
 var webpack = require('webpack');
 var WebpackDevServer = require('webpack-dev-server');
 var config = require('./webpack.config.js');
+var port = process.env.PORT || 3000;
 
 new WebpackDevServer(webpack(config), {
   publicPath: config.output.publicPath,
   hot: true,
   historyApiFallback: true
-}).listen(3000, '0.0.0.0', function (err, result) {
+}).listen(port, '0.0.0.0', function (err, result) {
   if (err) {
     console.log(err);
   }
