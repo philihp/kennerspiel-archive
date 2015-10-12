7077c625ab8ace217bc2c813ee49340e98f91c3c
Philihp Busby <philihp@gmail.com>
2015-10-12 05:41:47 -0700

    Elaborated documentation

--- deploy.js
+++ deploy.js
@@ -1,8 +1,9 @@
-// Since postinstall will also run when you run npm install
-// locally we make sure it only runs in production
+// This should only run on production, during an npm install.
+// When this runs, the bundle.js file will be built and dropped in /public/build/, and served
+// as a static asset, rather than on development where it is served with the hot-reloader hotness.
+//
+// Thanks to http://www.christianalfoni.com/articles/2015_04_19_The-ultimate-webpack-setup for this
 if (process.env.NODE_ENV === 'production') {
-    // We basically just create a child process that will run
-    // the production bundle command
     var child_process = require('child_process');
     child_process.exec("webpack -p --config webpack.production.config.js", function (error, stdout, stderr) {
         console.log('stdout: ' + stdout);
